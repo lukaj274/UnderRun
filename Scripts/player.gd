dede54813b069e326fb5903ff40b127d7aba4ef0
lukaj <jluka8082@gmail.com>
2026-02-28 20:09:52 -0800

    stuff

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,22 +4,20 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var mouse_location
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_pressed("move_forward"):
+		velocity.y = SPEED
 
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	mouse_location = get_global_mouse_position()
+	var angle = (mouse_location - global_position).angle()
+
+	rotation_degrees = angle
