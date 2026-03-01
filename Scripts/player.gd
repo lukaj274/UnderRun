41017d8c20493906e53881bf9de3e2e6ecb4027f
lukaj <jluka8082@gmail.com>
2026-03-01 14:41:07 -0800

    music related stuff

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,10 +2,6 @@ extends CharacterBody2D
 
 func _process(delta: float) -> void:
 	_movement(delta)
-	var audio_player = get_parent().get_node("AudioStreamPlayer2D")
-	if audio_player != null:
-		print("Playing music")
-		audio_player.play()
 
 func _movement(delta: float) -> void:
 	var cursor_pos := get_global_mouse_position()
