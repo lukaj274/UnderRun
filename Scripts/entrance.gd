b32a288db392c769a2277739bee29854a2a558e6
lukaj <jluka8082@gmail.com>
2026-03-01 13:43:19 -0800

    made entry change scenes

---
+++ Scripts/entrance.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var level_1: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Changing Scene...")
+	get_tree().change_scene_to_packed(level_1)
